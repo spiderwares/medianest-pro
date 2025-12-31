16c90f1f5e40b6a9771edbbb774b2261476152ef
dev1Cosmic <dev1.cosmic@outlook.com>
2025-12-31 11:44:11 +0530

    Add a documentation link in install file.

--- includes/class-wpmn-install-pro.php
+++ includes/class-wpmn-install-pro.php
@@ -20,6 +20,29 @@ if ( ! class_exists( 'WPMN_Install_Pro' ) ) :
          */
         public static function init() {
             add_filter( 'plugin_action_links_' . plugin_basename( WPMN_PRO_FILE ), array( __CLASS__, 'plugin_action_links' ) );
+            add_filter( 'plugin_row_meta', array( __CLASS__, 'plugin_row_meta' ), 10, 2 );
+        }
+
+        /**
+         * Add link to Documentation.
+         *
+         * @param array $links Array of action links.
+         * @param string $file Plugin file.
+         * @return array Modified array of action links.
+         */
+        public static function plugin_row_meta( $links, $file ) {
+            if ( plugin_basename( WPMN_PRO_FILE ) === $file ) :
+                $doc_url   = esc_url( 'https://documentation.cosmicinfosoftware.com/medianest/documents/getting-started/introduction/' );
+                $doc_label = esc_html( 'Documentation' );
+        
+                $new_links = array(
+                    '<a href="' . $doc_url . '" target="_blank">' . $doc_label . '</a>',
+                );
+        
+                $links = array_merge( $links, $new_links );
+            endif;
+        
+            return $links;
         }
 
         /**
